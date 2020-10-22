728378674599d36302928e27ce29e3d0a70ff586
Luke <LP030@atcltd.atc.org.nz>
2020-10-23 09:15:31 +1300

    Added Db script

---
+++ WebAPI/DbScripts/dbo.Vehicle.data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [dbo].[Vehicle] ON
+INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (3, N'LPN791', N'Mazda', N'Rx8', N'Black', 2003)
+INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (11, N'EJS784', N'Audi', N'A4', N'Blue', 1998)
+INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (15, N'Mewwwo', N'Tesla', N'3', N'White', 2019)
+INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (25, N'CCRAIG', N'Tesla', N'S', N'Grey', 2020)
+SET IDENTITY_INSERT [dbo].[Vehicle] OFF
