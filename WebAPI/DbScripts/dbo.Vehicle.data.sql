6bf5bccd642474b9205c08c48f830bfd71bdd9e2
luke <developmentdesignsltd@gmail.com>
2021-06-27 03:14:35 +1200

    Revert "small tweaks"
    This reverts commit 27e516e375cf433e2124cfc608d98a5d95b60e49.

--- WebAPI/DbScripts/dbo.Vehicle.data.sql
+++ WebAPI/DbScripts/dbo.Vehicle.data.sql
@@ -1,7 +1,6 @@
 ﻿SET IDENTITY_INSERT [dbo].[Vehicle] ON
-INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (1, N'LPN791', N'Mazda', N'Rx8', N'Black', 2003)
-INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (2, N'EJS784', N'Audi', N'A4', N'Blue', 1998)
-INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (3, N'Mewwwo', N'Tesla', N'3', N'White', 2019)
-INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (4, N'CCRAIG', N'Tesla', N'S', N'Grey', 2020)
-INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (5, N'EKL200', N'Toyota', N'Levin', N'Grey', 1996)
+INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (3, N'LPN791', N'Mazda', N'Rx8', N'Black', 2003)
+INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (11, N'EJS784', N'Audi', N'A4', N'Blue', 1998)
+INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (15, N'Mewwwo', N'Tesla', N'3', N'White', 2019)
+INSERT INTO [dbo].[Vehicle] ([Id], [Reg], [Make], [Model], [Colour], [Year]) VALUES (25, N'CCRAIG', N'Tesla', N'S', N'Grey', 2020)
 SET IDENTITY_INSERT [dbo].[Vehicle] OFF
